16df6b1fe6e217f581791b47c2cbe2ca03c9fd2f
Barnett Trzcinski <btrzcinski@gmail.com>
2017-12-02 14:00:40 -0800

    Added Day 1 part 1 solution

---
+++ src/day1_1.clj
@@ -0,0 +1,29 @@
+(ns day1-1
+  (:require [clojure.string :as string])
+  )
+
+(defn input-to-seq [x]
+  (map (fn [^Character x] (Integer/valueOf (.toString x)))
+     (.toCharArray x))
+  )
+
+(defn captcha-sum-from-seq [input]
+  (loop [loop-seq input
+         sum 0]
+    (if (second loop-seq)
+      (recur (rest loop-seq)
+             (if (= (first loop-seq) (second loop-seq))
+               (+ sum (first loop-seq))
+               sum))
+      sum)))
+
+(defn captcha-sum [input]
+  (captcha-sum-from-seq (cons (last (input-to-seq input)) (input-to-seq input))))
+
+(prn (captcha-sum "1122"))
+(prn (captcha-sum "1111"))
+(prn (captcha-sum "1234"))
+(prn (captcha-sum "91212129"))
+
+(def file-input (string/trim (slurp "../data/day1-1.txt")))
+(prn (captcha-sum file-input))

61b87d19d304281f27ae5be0ca654593c3866193
Barnett Trzcinski <btrzcinski@gmail.com>
2017-12-02 15:34:27 -0800

    Refactoring using thread-last macro

--- src/day2_1.clj
+++ src/day2_1.clj
@@ -6,7 +6,9 @@
   )
 
 (defn checksum [data]
-  (apply + (map #(- (apply max %) (apply min %)) data)))
+  (->> data
+       (map #(- (apply max %) (apply min %)))
+       (apply +)))
 
 (def test-data (input-to-seqs "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8\n"))
 (prn (checksum test-data))

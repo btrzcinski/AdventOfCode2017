756b6626db809afb86b91c53e4877715bb30e562
Barnett Trzcinski <btrzcinski@gmail.com>
2017-12-04 19:04:02 -0800

    Added a :refer to bring in function under test

--- src/day4_2_tests.clj
+++ src/day4_2_tests.clj
@@ -1,14 +1,14 @@
 (ns day4-2-tests
-  [:use clojure.test
-   :require day4-2])
+  (:use [clojure.test])
+  (:require [day4-2 :refer [valid-passphrase?]]))
 
 (deftest valid
-  (is (day4-2/valid-passphrase? "abcde fghij"))
-  (is (day4-2/valid-passphrase? "a ab abc abd abf abj"))
-  (is (day4-2/valid-passphrase? "iiii oiii ooii oooi oooo")))
+  (is (valid-passphrase? "abcde fghij"))
+  (is (valid-passphrase? "a ab abc abd abf abj"))
+  (is (valid-passphrase? "iiii oiii ooii oooi oooo")))
 
 (deftest invalid
-  (is (not (day4-2/valid-passphrase? "abcde xyz ecdab")))
-  (is (not (day4-2/valid-passphrase? "oiii ioii iioi iiio"))))
+  (is (not (valid-passphrase? "abcde xyz ecdab")))
+  (is (not (valid-passphrase? "oiii ioii iioi iiio"))))
 
 (run-tests)
